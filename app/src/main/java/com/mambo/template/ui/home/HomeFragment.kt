f2d69cbd1b9c4452c8efcc08c3fc22f91f8d7fc9
Brian <mambobryan@gmail.com>
2021-08-18 14:19:46 +0300

    feature-home: adds date and day to home - adds settings icon

--- app/src/main/java/com/mambo/template/ui/home/HomeFragment.kt
+++ app/src/main/java/com/mambo/template/ui/home/HomeFragment.kt
@@ -4,10 +4,13 @@ import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
 import com.mambo.template.R
 import com.mambo.template.databinding.FragmentHomeBinding
 import com.zhuinden.fragmentviewbindingdelegatekt.viewBinding
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collect
 
 @AndroidEntryPoint
 class HomeFragment : Fragment(R.layout.fragment_home) {
@@ -20,8 +23,23 @@ class HomeFragment : Fragment(R.layout.fragment_home) {
 
         binding.apply {
 
-            textView2.text = "Target ${viewModel.targetCalories} kcal"
+            tvHomeDay.text = "${viewModel.day.value}"
+            tvHomeDate.text = "${viewModel.date.value}"
 
+            ivHomeSettings.setOnClickListener {
+                viewModel.navigateToSettings()
+            }
+
+        }
+
+        viewLifecycleOwner.lifecycleScope.launchWhenStarted {
+            viewModel.homeEvent.collect { event ->
+                when (event) {
+                    HomeViewModel.HomeEvent.NavigateToSettings -> {
+                        findNavController().navigate(R.id.action_mainFragment_to_settingsFragment)
+                    }
+                }
+            }
         }
     }
 }
--- app/src/main/java/com/mambo/template/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/mambo/template/ui/home/HomeViewModel.kt
@@ -1,8 +1,20 @@
 package com.mambo.template.ui.home
 
+import android.os.Build
+import androidx.annotation.RequiresApi
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.mambo.template.ui.setup.SetupViewModel
 import com.mambo.template.utils.PreferenceManager
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.receiveAsFlow
+import kotlinx.coroutines.launch
+import java.text.SimpleDateFormat
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+import java.util.*
 import javax.inject.Inject
 
 @HiltViewModel
@@ -10,6 +22,34 @@ class HomeViewModel @Inject constructor(
     val preferenceManager: PreferenceManager
 ) : ViewModel() {
 
+    private val _homeEventChannel = Channel<HomeEvent>()
+    val homeEvent = _homeEventChannel.receiveAsFlow()
+
     val targetCalories = preferenceManager.getTargetCalories()
+
+    val day = MutableLiveData("Thursday")
+    val date = MutableLiveData("")
+
+    init {
+        getDate()
+    }
+
+    private fun getDate() {
+        val sdf = SimpleDateFormat("MMM dd, yyyy")
+        val currentDate = sdf.format(Date())
+
+        date.value = currentDate
+
+    }
+
+    fun navigateToSettings() {
+        viewModelScope.launch {
+            _homeEventChannel.send(HomeEvent.NavigateToSettings)
+        }
+    }
+
+    sealed class HomeEvent {
+        object NavigateToSettings : HomeEvent()
+    }
 
 }
